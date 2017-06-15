cca62df02735b2f23a019d03022b476e535faf70
Panicz <ka54mil@wp.pl>
2017-06-15 19:30:31 +0200

    Dodane id oraz tablica z wartośćiami przycisków

--- app/src/main/java/com/example/panicz/calculator/Controllers/CalculatorViewCreator.java
+++ app/src/main/java/com/example/panicz/calculator/Controllers/CalculatorViewCreator.java
@@ -2,6 +2,7 @@ package com.example.panicz.calculator.Controllers;
 
 import android.app.Activity;
 import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
@@ -12,10 +13,12 @@ public class CalculatorViewCreator {
     private Activity activity;
     private GridLayout gridButtons;
     private DisplayMetrics displayMetrics;
+    private String values [];
 
     public CalculatorViewCreator(Activity activity){
 
         this.activity = activity;
+        values = new String[]{"1", "2", "3", "+", "-", "4", "5", "6", "*", "/", "7", "8", "9", "0", "^", "( )",".", "+/-", "C", "="};
         displayMetrics = new DisplayMetrics();
         gridButtons = (GridLayout)activity.findViewById(R.id.GridButtons);
         activity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -37,14 +40,23 @@ public class CalculatorViewCreator {
     private void createHorizontalView(GridLayout gridButtons){
         EditText equationTextField = new EditText(activity);
         setAttributesToTextField(equationTextField);
-        for (int i = 0; i < 10; i++) {
-            gridButtons.addView(new Button(activity));
+        for (int i = 0; i < values.length; i++) {
+
+            Button tmp = new Button(activity);
+            tmp.setId(activity.getResources().getIdentifier("button"+i, "id", activity.getPackageName()));
+            tmp.setText(values[i]);
+            gridButtons.addView(tmp);
         }
     }
 
     private void setAttributesToTextField(EditText textField){
+        GridLayout.LayoutParams params = new GridLayout.LayoutParams();
+        params.columnSpec = GridLayout.spec(0,5);
+        textField.setLayoutParams(params);
         textField.setText("0");
+        textField.setGravity(Gravity.CLIP_HORIZONTAL|Gravity.TOP|Gravity.BOTTOM);
         textField.setId(R.id.equationTextField);
+        gridButtons.setPadding(5,0,5,0);
         gridButtons.addView(textField);
     }
 
