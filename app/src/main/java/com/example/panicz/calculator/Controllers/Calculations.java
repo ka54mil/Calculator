15927c54bbc8f5137f297951bc1fb2df8f0f6cf7
Panicz <ka54mil@wp.pl>
2017-06-16 21:01:28 +0200

    gCD

--- app/src/main/java/com/example/panicz/calculator/Controllers/Calculations.java
+++ app/src/main/java/com/example/panicz/calculator/Controllers/Calculations.java
@@ -5,40 +5,71 @@ import java.math.BigDecimal;
 public class Calculations {
 
     public BigDecimal add(BigDecimal first, BigDecimal second) {
-        return first.add(second).stripTrailingZeros();
+        return first.add(second);
     }
 
     public BigDecimal subtract(BigDecimal first, BigDecimal second) {
-        return first.subtract(second).stripTrailingZeros();
+        return first.subtract(second);
     }
 
     public BigDecimal multiply(BigDecimal first, BigDecimal second) {
-        return first.multiply(second).stripTrailingZeros();
+        return first.multiply(second);
     }
 
     public BigDecimal divide(BigDecimal first, BigDecimal second) throws ArithmeticException {
         if (second.equals(new BigDecimal(0))) {
             throw new ArithmeticException("Dividing by 0");
         }
-        return first.divide(second, 350, BigDecimal.ROUND_HALF_UP).stripTrailingZeros();
+        return first.divide(second, 350, BigDecimal.ROUND_HALF_UP).setScale(300,BigDecimal.ROUND_HALF_UP);
     }
 
 //    public BigDecimal pow(BigDecimal first, BigDecimal n){
 //        BigDecimal bigDecimalToN = n.stripTrailingZeros().remainder(BigDecimal.ONE).movePointRight(first.scale()); //Muszę zamienic np. 22312.4312 na ulamek i dodac potege i pierwiastek
 //        int powPart = n.intValue();
 //        if (n.subtract(n.int).compareTo(powPart))
-//        return first.pow().add(root(first,)).stripTrailingZeros();
+//        return first.pow().add(root(first,));
 //    }
 
     public BigDecimal pow(BigDecimal first, int n) {
-        return first.pow(n).stripTrailingZeros();
+        return first.pow(n);
+    }
+
+    public String rootI(String first, int n){// do zwracania wartosci z i
+        return new String();
     }
 
     public BigDecimal root(BigDecimal first, int n) {
 
-        BigDecimal result = new BigDecimal(10).setScale(300, BigDecimal.ROUND_HALF_UP);
+        first = first.setScale(300,BigDecimal.ROUND_HALF_UP);
+        BigDecimal addition = divide(first, BigDecimal.TEN);
+        BigDecimal result = addition;
+        BigDecimal resultPow = pow(result, n);
+        int compare;
 
+        while((compare = first.compareTo(resultPow))!=0){
+            if(compare!=first.signum()){
+                result = result.subtract(addition);
+                addition = divide(addition, BigDecimal.TEN);
+            } else {
+                result = add(result, addition);
+            }
+            resultPow = pow(result, n);
+        }
 
-        return result.stripTrailingZeros();
+        if(n % 2 == 1 && first.signum() == -1){
+            if (result.signum() == 1) {
+                return result.negate();
+            }
+        }
+        return result;
+    }
+
+    public BigDecimal gCD(BigDecimal first, BigDecimal second){
+        BigDecimal rest;
+        while((rest = first.remainder(second)).compareTo(BigDecimal.ZERO)!=0){
+            first = second;
+            second = rest;
+        }
+        return second.abs();
     }
 }
--- app/src/test/java/com/example/panicz/calculator/CalculationsUnitTest.java
+++ app/src/test/java/com/example/panicz/calculator/CalculationsUnitTest.java
@@ -9,6 +9,14 @@ import java.math.BigDecimal;
 
 public class CalculationsUnitTest {
     private Calculations test = new Calculations();
+    private BigDecimal minusJedenTrzyPiecSiedemPiecSzescSiedemOsiemDziewiecZeroPiecDZiewiecZeroZeroOsiemDziewiecPiecJedenJedenDziewiecZeroZeroOsiemSzesc = new BigDecimal("-246456789104008951190086");
+    private BigDecimal minusJedenDwaTrzyCzteryPiecSzescSiedemOsiemDziewiecZeroPiecCzteryZeroZeroZeroDziewiecPiecJedenJedenZeroZeroZeroJedenOsiemSzesc = new BigDecimal("-123456789054000951100086");
+    private BigDecimal minusJedenDwaTrzyZeroZeroZeroZeroZeroZeroZeroPiecZeroZeroZeroOsiemZeroZeroZeroZeroDziewiecZeroZeroZeroZero = new BigDecimal("-123000000050008000090000");
+    private BigDecimal minusSzescCztery = new BigDecimal("-64");
+    private BigDecimal minusCztery = new BigDecimal("-4");
+    private BigDecimal minusZero00000000000000000001 = new BigDecimal("-0.00000000000000000001");
+    private BigDecimal zero = new BigDecimal("0");
+    private BigDecimal zero00000000000000000001 = new BigDecimal("0.00000000000000000001");
     private BigDecimal jeden = new BigDecimal("1");
     private BigDecimal dwa = new BigDecimal("2");
     private BigDecimal dwa34 = new BigDecimal("2.34");
@@ -21,32 +29,32 @@ public class CalculationsUnitTest {
     private BigDecimal dwaTrzy4 = new BigDecimal("23.4");
     private BigDecimal trzyDwa = new BigDecimal("32");
     private BigDecimal szescCztery = new BigDecimal("64");
+    private BigDecimal jedenSzescDziewiecSiedemSiedemPiecCzteryZeroOsiemSzescTrzyDwaJedenCzteryDiewiecPiecTrzyOsiemOsiemDziewiecJeden = new BigDecimal("169775408632149538891");
+    private BigDecimal jedenPiecCzteryOsiemDwaJedenDziewiecSzescSiedemSzescDziewiecTrzyTrzyZeroDziwecCzteryPiecZeroZeroJedenOsiemSiedemJeden = new BigDecimal("15482196769330945001871");
+    private BigDecimal jedenPiecSzescPiecJedenDziewiecSiedemDwaJedenSiedemSiedemDziewiecSzescTrzyZeroDziewiecCzteryPiecCzteryZeroSiedemSzescDwa = new BigDecimal("15651972177963094540762");
 
     @Test
     public void addTest(){
-        Assert.assertEquals(cztery, test.add(dwa,dwa));
-        Assert.assertEquals(dwaTrzy4,test.add(dwaJeden06,dwa34));
+        Assert.assertEquals(cztery, test.add(dwa,dwa).stripTrailingZeros());
+        Assert.assertEquals(dwaTrzy4,test.add(dwaJeden06,dwa34).stripTrailingZeros());
+        Assert.assertEquals(jedenPiecSzescPiecJedenDziewiecSiedemDwaJedenSiedemSiedemDziewiecSzescTrzyZeroDziewiecCzteryPiecCzteryZeroSiedemSzescDwa, test.add(jedenPiecCzteryOsiemDwaJedenDziewiecSzescSiedemSzescDziewiecTrzyTrzyZeroDziwecCzteryPiecZeroZeroJedenOsiemSiedemJeden, jedenSzescDziewiecSiedemSiedemPiecCzteryZeroOsiemSzescTrzyDwaJedenCzteryDiewiecPiecTrzyOsiemOsiemDziewiecJeden).stripTrailingZeros());
+        Assert.assertEquals(zero, test.add(zero00000000000000000001,minusZero00000000000000000001).stripTrailingZeros());
+        Assert.assertEquals(minusJedenTrzyPiecSiedemPiecSzescSiedemOsiemDziewiecZeroPiecDZiewiecZeroZeroOsiemDziewiecPiecJedenJedenDziewiecZeroZeroOsiemSzesc, test.add(minusJedenDwaTrzyCzteryPiecSzescSiedemOsiemDziewiecZeroPiecCzteryZeroZeroZeroDziewiecPiecJedenJedenZeroZeroZeroJedenOsiemSzesc,minusJedenDwaTrzyZeroZeroZeroZeroZeroZeroZeroPiecZeroZeroZeroOsiemZeroZeroZeroZeroDziewiecZeroZeroZeroZero).stripTrailingZeros());
     }
 
     @Test
     public void rootTest(){
-        BigDecimal x0 = szescCztery;
-        int n = 2;
-        BigDecimal x1 = x0.divide(jedenZero);
-        BigDecimal x2 = x1;
-        BigDecimal x3 = x2.pow(n).setScale(300, BigDecimal.ROUND_HALF_UP);
+        Assert.assertEquals(osiem, test.root(szescCztery, 2).stripTrailingZeros());
+        Assert.assertEquals(cztery, test.root(szescCztery, 3).stripTrailingZeros());
+        Assert.assertEquals(minusCztery, test.root(minusSzescCztery, 3).stripTrailingZeros());
+        Assert.assertEquals(dwa, test.root(szescCztery, 6).stripTrailingZeros());
+    }
 
-        while(!x0.setScale(300, BigDecimal.ROUND_HALF_UP).equals(x3)){
-            if(x3.compareTo(x0)==1){
-                x2 = x2.subtract(x1);
-                x1 = x1.divide(jedenZero, 350, BigDecimal.ROUND_HALF_UP);
-            } else {
-                x2 = x2.add(x1);
-            }
-            x3 = x2.pow(n).setScale(300, BigDecimal.ROUND_HALF_UP);
-        }
-
-        Assert.assertEquals(osiem.toPlainString(), x2.stripTrailingZeros().toPlainString());
-
+    @Test
+    public void gCDTest(){
+        Assert.assertEquals(osiem, test.gCD(szescCztery, osiem).stripTrailingZeros());
+        Assert.assertEquals(cztery, test.gCD(szescCztery, minusCztery).stripTrailingZeros());
+        Assert.assertEquals(dwa, test.gCD(minusSzescCztery, dwa).stripTrailingZeros());
+        Assert.assertEquals(cztery, test.gCD(cztery, minusCztery).stripTrailingZeros());
     }
 }
